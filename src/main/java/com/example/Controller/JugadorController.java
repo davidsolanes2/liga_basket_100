f9d4bb531023bd5c423edb7d04b64a5381314890
davidsolanes2 <davidsolanes2@gmail.com>
2016-11-15 12:40:26 +0100

    primera parte ejercicio

--- src/main/java/com/example/Controller/JugadorController.java
+++ src/main/java/com/example/Controller/JugadorController.java
@@ -14,10 +14,12 @@ import org.springframework.test.annotation.Timed;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.OPTIONS;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
@@ -28,26 +30,47 @@ public class JugadorController {
     @Autowired
     private JugadorRepository jugadorRepository;
 
-    @RequestMapping(method = POST)
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Jugador save(@RequestBody Jugador jugador) {
         return jugadorRepository.save(jugador);
     }
 
-    @RequestMapping(method = GET)
-    public List<Jugador>findAll() {
+    @GetMapping
+    public List<Jugador> getAllJugador() {
         List<Jugador> jugadors = new ArrayList<>();
-        jugadors.addAll(jugadorRepository.findAll());
+        Iterator<Jugador> iterator = jugadorRepository.findAll().iterator();
+        while (iterator.hasNext()) {
+         jugadors.add(iterator.next());
+        }
         return jugadors;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping("/{id")
     public void deletePlayerId(@PathVariable Long id) {
         Jugador jugador = jugadorRepository.findOne(id);
-        if (jugador != null) jugadorRepository.delete(id);
+        if (jugador != null)jugadorRepository.delete(id);
     }
 
-    @RequestMapping(value = "/{id}" , method = RequestMethod.PUT)
+    @PutMapping
+    public Jugador updateJugador(@RequestBody Jugador jugador) {
+        return jugadorRepository.save(jugador);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Jugador> getJugadorID(@PathVariable Long id) {
+        Jugador jugador = jugadorRepository.findOne(id);
+        return Optional.ofNullable(jugador)
+                .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/byNumCanastas/")
+    public List<Jugador> findByNumCanastasGreaterThanEqual(@PathVariable Integer numCanastas) {
+        return jugadorRepository.findByNumCanastasGreaterThanEqual(numCanastas);
+    }
+
+    /*@RequestMapping(value = "/{id}" , method = RequestMethod.PUT)
     public Jugador updateJugadorId(@PathVariable Long id, @RequestBody Jugador jugador) {
         Jugador j = jugadorRepository.findOne(id);
         if (j == jugador) return null;
@@ -61,7 +84,7 @@ public class JugadorController {
         return Optional.ofNullable(jugador)
                 .map(result -> new ResponseEntity<>(result, HttpStatus.OK))
                 .orElse(new ResponseEntity<Jugador>(HttpStatus.NOT_FOUND));
-    }
+    }*/
 
 
 }
